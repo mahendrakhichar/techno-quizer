23a77d84cdc5fe3a63c9020bb2bdd7ac9df6bd9d
techno <2021kuec2049@iiitkota.ac.in>
2025-03-13 00:08:54 +0530

    update some code

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000',
-        target:'https://techno-quizer-2.onrender.com/',
+        target: 'http://localhost:5000',
+        // target:'https://techno-quizer-2.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
